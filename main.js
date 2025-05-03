4f2dcd927f6d2c960a69d233528c40b25da904e8
Mst. Musfika Akter <akmusfika.dev@gmail.com>
2025-05-03 09:58:15 +0600

    fixed random number bug and show previous guess

--- main.js
+++ main.js
@@ -7,11 +7,59 @@ let score = 0;
 let chance = 10;
 let userInputs = [];
 let comInput;
+let range;
+
+/// reset game
+document.querySelector(".reset").addEventListener("click", () => {
+  location.reload();
+  userInput.value = "";
+});
+
+/// play again
+const modal = document.querySelector(".modal_box");
+
+document.querySelector(".play_again").addEventListener("click", () => {
+  modal.classList.remove("show");
+  location.reload();
+  userInput.value = "";
+});
+
+/// see previous guesses
+const preGuessLink = document.querySelector("#previous_guess");
+const showPreviousGuess = document.querySelector(".show_guesses");
+
+preGuessLink.addEventListener("click", () => {
+  showPreviousGuess.textContent = userInputs.join(", ");
+});
+
+/// show range
+const showRange = document.querySelector(".game_range-box");
+
+window.addEventListener("load", () => {
+    showRange.classList.add("show");
+    userInput.value = "";
+});
+
+document.querySelector(".choose_range").addEventListener("click", () => {
+    showRange.classList.add("show");
+});
+
+/// choose range
+const rangeBtn = document.querySelectorAll(".range");
+const topMsg = document.querySelector("#range_show");
+
+rangeBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+    showRange.classList.remove("show");
+        range = parseInt(btn.textContent);
+        topMsg.textContent = `1 to ${range}`;
+    })
+});
 
 /// play game
 submitBtn.addEventListener("click", () => {
-  comInput = Math.floor(Math.random() * 10 + 1);
   const userVal = parseInt(userInput.value.trim());
+  comInput = Math.floor(Math.random() * range + 1);
 
   // check if the input is empty
   if (isNaN(userVal)) {
@@ -35,56 +83,9 @@ submitBtn.addEventListener("click", () => {
   }
 
   // check user chance
-  if (userInputs.length === 10) {
-      modal.classList.add("show");
-      console.log("no more chance");
-  }
+  // if (userInputs.length === 10) {
+  //     modal.classList.add("show");
+  //     console.log("no more chance");
+  // }
 
-});
-
-/// reset game
-document.querySelector(".reset").addEventListener("click", () => {
-  location.reload();
-  userInput.value = "";
-});
-
-/// play again
-const modal = document.querySelector(".modal_box");
-
-document.querySelector(".play_again").addEventListener("click", () => {
-  modal.classList.remove("show");
-  location.reload();
-  userInput.value = "";
-});
-
-/// see previous guesses
-const previousGuess = document.querySelector("#previous_guess");
-previousGuess.addEventListener("click", () => {
-  console.log(userInputs);
-  previousGuess.textContent = userInputs.join(", ");
-});
-
-/// show range
-const showRange = document.querySelector(".game_range-box");
-
-window.addEventListener("load", () => {
-    showRange.classList.add("show");
-    userInput.value = "";
-});
-
-document.querySelector(".choose_range").addEventListener("click", () => {
-    showRange.classList.add("show");
-});
-
-/// choose range
-const rangeBtn = document.querySelectorAll(".range");
-const topMsg = document.querySelector("#range_show");
-
-rangeBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-    showRange.classList.remove("show");
-        let range = parseInt(btn.textContent);
-        comInput = Math.floor(Math.random() * range + 1);
-        topMsg.textContent = `1 to ${range}`;
-    })
 });
